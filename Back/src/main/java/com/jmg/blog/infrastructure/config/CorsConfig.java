39dbdf3b49dc0eef3df65caa8947e507c5b4f6b0
SSAFY <jmg97@kakao.com>
2024-02-23 09:48:43 +0900

    update : 콜스 세팅 업데이트

--- Back/src/main/java/com/jmg/blog/infrastructure/config/CorsConfig.java
+++ Back/src/main/java/com/jmg/blog/infrastructure/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("http://localhost:4173");
+        config.addAllowedOrigin("https://mgbg.kr");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
