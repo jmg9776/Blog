0a0920d8416e53dbcb2392be74d18c325429e14b
SSAFY <jmg97@kakao.com>
2024-02-25 22:16:23 +0900

    Feat : 라우터 이동시 스크롤 이동 추가 - 메인페이지 이동시 최상단 이동 - 이외 페이지 이동시 컨텐츠 바로 이동

--- Front/src/router/index.ts
+++ Front/src/router/index.ts
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from '@/features/home/Home.vue';
 import Board from "@/features/board/Board.vue";
+import {nextTick} from "vue";
 
 const routes = [
     {path: '/', component: Home},
@@ -10,6 +11,25 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, _, savedPosition) {
+        if (savedPosition) {
+            return { ...savedPosition, behavior: 'smooth' };
+        } else {
+            return new Promise((resolve) => {
+                nextTick(() => {
+                    let scrollPosition = 0;
+                    if (to.path !== '/') {
+                        scrollPosition = window.innerHeight;
+                    }
+                    resolve({
+                        top: scrollPosition,
+                        behavior: 'smooth'
+                    });
+                });
+            });
+        }
+    },
 });
+
 
 export default router;
